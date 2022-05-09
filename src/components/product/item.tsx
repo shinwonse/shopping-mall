77ce764e905a070d6e4fd2f6579d4ebebd63538b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-09 23:43:00 +0900

    product 컴포넌트

---
+++ src/components/product/item.tsx
@@ -0,0 +1,22 @@
+import { Product } from "../../types";
+
+const ProductItem = ({
+  category,
+  description,
+  id,
+  image,
+  price,
+  rating,
+  title,
+}: Product) => (
+  <li className="product-item">
+    {/* <p className="product-item__category">{category}</p> */}
+    <p className="product-item__title">{title}</p>
+    {/* <p className="product-item__description">{description}</p> */}
+    <img className="product-item__image" src={image} />
+    <span>${price}</span>
+    <span className="product-item__rating">{rating.rate}</span>
+  </li>
+);
+
+export default ProductItem;
