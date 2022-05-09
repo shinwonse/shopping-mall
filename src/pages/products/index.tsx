756d3ad8f4f72ecb4d86569901e556ce4f48698e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-09 23:43:08 +0900

    products 페이지

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,25 @@
-const ProductList = () => <div>상품목록</div>;
+import { useQuery } from "react-query";
+import ProductItem from "../../components/product/item";
+import { fetcher, QueryKeys } from "../../queryClient";
+import { Product } from "../../types";
+
+const ProductList = () => {
+  const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () =>
+    fetcher({
+      method: "GET",
+      path: "/products",
+    })
+  );
+
+  return (
+    <div>
+      <ul className="products">
+        {data?.map((product) => (
+          <ProductItem {...product} key={product.id} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ProductList;
