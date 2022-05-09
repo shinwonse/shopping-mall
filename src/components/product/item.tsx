fc023bcfea56ce7c5b01829dda46731be77afa45
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-05-10 00:59:10 +0900

    제품목록, 상세페이지 업데이트

--- src/components/product/item.tsx
+++ src/components/product/item.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Product } from "../../types";
 
 const ProductItem = ({
@@ -10,12 +11,14 @@ const ProductItem = ({
   title,
 }: Product) => (
   <li className="product-item">
-    {/* <p className="product-item__category">{category}</p> */}
-    <p className="product-item__title">{title}</p>
-    {/* <p className="product-item__description">{description}</p> */}
-    <img className="product-item__image" src={image} />
-    <span>${price}</span>
-    <span className="product-item__rating">{rating.rate}</span>
+    <Link to={`/products/${id}`}>
+      {/* <p className="product-item__category">{category}</p> */}
+      <p className="product-item__title">{title}</p>
+      {/* <p className="product-item__description">{description}</p> */}
+      <img className="product-item__image" src={image} />
+      <span>${price}</span>
+      <span className="product-item__rating">{rating.rate}</span>
+    </Link>
   </li>
 );
 
